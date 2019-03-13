121dbf58539b323e1f7b785ecc687a57cd9816cd
Kestutis <kestas1393@gmail.com>
2019-03-13 22:51:06 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      movies: [],
+      text: '',
+      matches: []
+    }
+  }
+
+  componentDidMount(searchTerm){
+    let initialMovies = [];
+    const urlString = "https://api.themoviedb.org/3/search/movie?api_key=6151647ec82570f6d554e7709c136a36&language=en-US&query=" + searchTerm + "&page=1&include_adult=false";
+    fetch(urlString).then(response => 
+      response.json())
+      .then(data => {
+        initialMovies = data.results.map((movie) => {
+        return movie
+      });
+        this.setState({
+          movies: initialMovies
+        });
+      });
+  }
+  
+  searchChangeHandler(event){
+
+    const searchTerm = event.target.value;
+    let matches = [];
+    this.setState({
+      movies: [], 
+      text: searchTerm,
+      matches: matches}, () => {
+      if (searchTerm.length >= 3) {
+        this.componentDidMount(searchTerm); 
+      } else {
+        this.setState({
+          movies: []
+        })
+      }
+    });
+  }
+
+  selectedSuggestion(value){
+    this.setState({
+        text: value,
+        movies: []
+    });
+  }
+  renderSuggestions(){
+      const {movies} = this.state;
+      if (movies.length === 0){
+          return null;
+      } 
+  }
   render() {
+    const {movies} = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <input style={{
+          display: 'block',
+          paddingLeft: 16,
+          paddingBottom: 8,
+          paddingTop: 8,
+          width: '50%',
+        }} value={this.state.text} onChange={this.searchChangeHandler.bind(this)} placeholder="Enter movie name"/> 
+            <ul className="list">
+              {movies.map(movie => 
+                <div key={movie.id}>
+                  <li key={movie.id} onClick={() => this.selectedSuggestion(movie.title)}>{movie.title}</li>
+                  <p>{movie.vote_average} Rating, {movie.release_date}</p>
+                </div>
+              )}
+            </ul>   
+            {this.renderSuggestions()}
       </div>
     );
   }
