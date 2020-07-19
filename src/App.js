de1a746b96ee9278fca7be8b99ea98c4a0aceb2c
Kestutis1993 <kestutis.pajaujis93@gmail.com>
2020-07-19 17:51:06 +0300

    styled header

--- src/App.js
+++ src/App.js
@@ -7,37 +7,32 @@ class App extends Component {
     this.state = {
       movies: [],
       text: '',
-      matches: []
+      // matches: []
     }
   }
 
   componentDidMount(searchTerm){
-    let initialMovies = [];
     const urlString = "https://api.themoviedb.org/3/search/movie?api_key=6151647ec82570f6d554e7709c136a36&language=en-US&query=" + searchTerm + "&page=1&include_adult=false";
     fetch(urlString).then(response => 
       response.json())
       .then(data => {
         console.log(data);
-        initialMovies = data.results.map((movie) => {
-          console.log(initialMovies);
-          console.log(movie);
-        return movie;
-        
-      });
+        // initialMovies = data.results.map((movie) => {
+        //   console.log(initialMovies);
+        //   console.log(movie);
+        // return movie;
         this.setState({
-          movies: initialMovies
+          movies: data.results.slice(0,8)
         });
       });
   }
   
   searchChangeHandler(event){
-
     const searchTerm = event.target.value;
-    let matches = [];
+    // let matches = [];
     this.setState({
       movies: [], 
-      text: searchTerm,
-      matches: matches}, () => {
+      text: searchTerm}, () => {
       if (searchTerm.length >= 3) {
         this.componentDidMount(searchTerm); 
       } else {
@@ -54,32 +49,43 @@ class App extends Component {
         movies: []
     });
   }
-  renderSuggestions(){
-      const {movies} = this.state;
-      if (movies.length === 0){
-          return null;
-      } 
-  }
+  // renderSuggestions(){
+  //     const {movies} = this.state;
+  //     if (movies.length === 0){
+  //         return null;  
+  //     } 
+  // }
+
   render() {
     const {movies} = this.state;
     return (
-      <div>
-        <input style={{
-          display: 'block',
-          paddingLeft: 16,
-          paddingBottom: 8,
-          paddingTop: 8,
-          width: '50%',
-        }} value={this.state.text} onChange={this.searchChangeHandler.bind(this)} placeholder="Enter movie name"/> 
+      <div className="header">
+        <form style={{padding: '25px 25px 15px'}}>
+          <div style={{display: 'inline'}}>
+            <div className="textbox">
+              <i className="fas fa-film fa-lg" />
+              <input value={this.state.text} 
+                onChange={this.searchChangeHandler.bind(this)}
+                placeholder="Enter movie name"
+              />
+            </div>
+            <div className="searchbox">
+              <div className="square"><i className="fas fa-search fa-2x"></i></div>
+            </div>
+          </div>
+           
+          <div> 
             <ul className="list">
-              {movies.map(movie => 
+              {movies.map(movie =>
                 <div key={movie.id}>
-                  <li key={movie.id} onClick={() => this.selectedSuggestion(movie.title)}>{movie.title}</li>
+                  <li onClick={() => this.selectedSuggestion(movie.title)}>{movie.title}</li>
                   <p>{movie.vote_average} Rating, {movie.release_date}</p>
                 </div>
               )}
             </ul>   
-            {this.renderSuggestions()}
+            {/* {this.renderSuggestions()} */}
+          </div>
+        </form>
       </div>
     );
   }
